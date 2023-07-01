17ac6a3df74bf98e17366d98462091e33485600c
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-01 17:49:18 +0300

    Create models

---
+++ app/models/post.rb
@@ -0,0 +1,25 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  after_save :update_posts_count
+
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_count
+    author.increment!(:post_counter)
+  end
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comment_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'must be an integer greater than or equal to zero.' }
+  validates :likes_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'must be an integer greater than or equal to zero.' }
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  validates :name, presence: true
+  validates :post_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'must be an integer greater than or equal to zero' }
+end
