ac7eda63072c9ac00c7e5bc1d4a9dbca221d3fac
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-02 20:00:51 +0300

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'validation' do
-    user = User.create(name: 'Amaka', photo: 'live photo', bio: 'live bio', post_counter: 0)
-    post = Post.create(title: 'My post', text: 'This is my first post', author_id: user.id, comment_counter: 0,
-                       likes_counter: 0)
+    subject do
+      described_class.new(text: 'This is my comment',
+                          post: Post.new(title: 'My post', text: 'This is my first post',
+                                         author: User.new(name: 'John', photo: 'john_photo.jpg',
+                                                          bio: 'Passionate about blogging',
+                                                          post_counter: 0), comment_counter: 0,
+                                         likes_counter: 0),
+                          author: User.new(name: 'Jane', photo: 'jane_photo.jpg', bio: 'Loves to share opinions',
+                                           post_counter: 0))
+    end
 
-    subject { described_class.create(text: 'This is my comment', post:, author_id: user.id) }
+    before { subject.save }
 
     it 'should be valid without a text' do
       subject.text = nil
@@ -14,7 +21,7 @@ RSpec.describe Comment, type: :model do
     end
 
     it 'posts comments count should increase' do
-      expect(subject.post.comment_counter).to eq(post.comment_counter)
+      expect(subject.post.comment_counter).to eq(1)
     end
   end
 end
