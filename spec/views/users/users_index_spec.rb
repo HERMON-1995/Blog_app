14dfdd2333ef34a6ed070217dedb57683540a365
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-03 21:05:57 +0300

    Update users_index_spec.rb

--- spec/views/users/users_index_spec.rb
+++ spec/views/users/users_index_spec.rb
@@ -3,9 +3,12 @@ require 'capybara/rspec'
 
 RSpec.describe 'User', type: :feature, js: false do
   before do
-    # Create some sample users for testing
-    User.create(name: 'John', photo: 'https://example.com/john.jpg')
-    User.create(name: 'Alice', photo: 'https://example.com/alice.jpg')
+    # Create some sample users for testing with unique email addresses
+    user1 = User.create(name: 'John', email: 'john@example.com', photo: 'https://example.com/john.jpg')
+    user2 = User.create(name: 'Alice', email: 'alice@example.com', photo: 'https://example.com/alice.jpg')
+    Post.create(title: 'Post 1', text: 'Content 1', author_id: user1.id)
+    Post.create(title: 'Post 2', text: 'Content 2', author_id: user1.id)
+    Post.create(title: 'Post 3', text: 'Content 3', author_id: user2.id)
   end
 
   it 'displays the list of users' do
@@ -15,6 +18,19 @@ RSpec.describe 'User', type: :feature, js: false do
     expect(page).to have_content('Alice')
   end
 
+      it 'I can see the number of posts each user has written' do
+        visit '/users'
+        expect(page).to have_content('Number of posts: 2', count: 1)
+        expect(page).to have_content('Number of posts: 1', count: 1)
+      end
+
+      it 'I can see the profile picture for each user' do
+        visit '/users'
+      
+        expect(page).to have_selector("img[src='https://example.com/john.jpg']")
+        expect(page).to have_selector("img[src='https://example.com/alice.jpg']")
+      end
+  
   it 'links to user show pages' do
     visit '/users'
 
@@ -25,7 +41,7 @@ RSpec.describe 'User', type: :feature, js: false do
   end
 
   it 'displays user information on show page' do
-    user = User.create(name: 'Jane', photo: 'https://example.com/jane.jpg')
+    user = User.create(name: 'Jane', email: 'jane@example.com', photo: 'https://example.com/jane.jpg')
 
     visit "/users/#{user.id}"
 
